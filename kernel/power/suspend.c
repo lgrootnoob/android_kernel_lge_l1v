cf51753b7bdbc90f6bea604688e37fd39477b6b8
lgrootnoob <lgrootnoob@github.com>
2014-10-21 19:21:17 -0500

    bringup to 3.4.1

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -183,6 +183,8 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	arch_suspend_disable_irqs();
 	BUG_ON(!irqs_disabled());
 
+	system_state = SYSTEM_SUSPEND;
+
 	error = syscore_suspend();
 	if (!error) {
 		*wakeup = pm_wakeup_pending();
@@ -195,6 +197,8 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		sleep_enter = 1;
 #endif
 	}
+
+	system_state = SYSTEM_RUNNING;
 
 	arch_suspend_enable_irqs();
 	BUG_ON(irqs_disabled());
